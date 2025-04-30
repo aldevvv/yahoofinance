146cf02ef5a5cf9326299e9be74daf27c3b5a340
Muhammad Alif <115059436+aldevvv@users.noreply.github.com>
2025-04-30 14:52:19 +0800

    Update Feature - Duration Limit Indicator

--- ysi.py
+++ ysi.py
@@ -27,7 +27,6 @@ def fetch_and_clean_history(ticker: str) -> pd.DataFrame:
         df["Date"] = pd.to_datetime(df["Date"]).dt.tz_localize(None)
     df = df[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
     return df
-
 def convert_df_to_excel(df: pd.DataFrame, ticker: str):
     from io import BytesIO
     output = BytesIO()
@@ -43,6 +42,7 @@ def convert_df_to_excel(df: pd.DataFrame, ticker: str):
         worksheet.set_column('G:G', 15, vol_fmt)
     output.seek(0)
     return output
+st.set_page_config(page_title="Yahoo Finance Historical Data - YSI")
 st.title("📈 Yahoo Finance Historical Data Downloader")
 st.markdown(
     """
@@ -61,8 +61,8 @@ url = st.text_input(
 )
 
 if url:
-    reset = st.session_state.get('limit_reset', 0)
-    remaining = reset - time.time()
+    reset_time = st.session_state.get('limit_reset', 0)
+    remaining = reset_time - time.time()
     if remaining > 0:
         mins, secs = divmod(int(remaining), 60)
         st.warning(f"⚠️ Rate limit masih aktif. Coba lagi dalam {mins}m {secs}s")
@@ -70,12 +70,10 @@ if url:
 
     ticker = extract_ticker_from_url(url)
     st.success(f"✅ Berhasil mendeteksi kode perusahaan/saham: {ticker}")
-
     try:
         with st.spinner("⏳ Sedang menjalankan program untuk mengambil data..."):
             df = fetch_and_clean_history(ticker)
     except YFRateLimitError:
-        # Set waktu tunggu 1 jam
         st.session_state['limit_reset'] = time.time() + 3600
         st.error("❌ Anda telah mencapai batas akses Yahoo Finance (Rate Limit). Harap tunggu beberapa menit dan coba lagi.")
         st.stop()
@@ -83,7 +81,7 @@ if url:
         st.error(f"❌ Terjadi kesalahan saat mengambil data: {e}")
         st.stop()
 
-    st.markdown("**🗂️ Preview Tabel 5 Baris Terakhir dari Data Historis yang Diambil (dari data terlama ke terbaru):**")
+    st.markdown("**🗂️ Preview 5 Baris Terakhir dari Data Historis yang Diambil (dari data terlama ke terbaru):**")
     st.dataframe(df.head())
 
     st.write(f"📊 Jumlah Baris Data History (Max): {len(df)}")
@@ -108,7 +106,7 @@ st.markdown("---")
 st.markdown(
     """
     <div style='text-align: center; font-size: 14px;'>
-        © 2025 | Developed with ❤️ by AlDev - Muhammad Alif<br>
+        © 2025 | Developed dengan ❤️ oleh AlDev - Muhammad Alif<br>
         Jika menemukan bug atau ingin memberikan saran, hubungi saya di <a href='https://www.instagram.com/mhdalif.id/' target='_blank'>@mhdalif.id</a>
     </div>
     """,
